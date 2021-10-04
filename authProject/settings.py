812b7bb2a213f425de94c504629c2916d9d2b238
David Barbosa <david.barbosa@mercadolibre.com.co>
2021-10-04 07:33:21 -0500

    Deploy and Routes

--- authProject/settings.py
+++ authProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 from pathlib import Path
 from datetime import timedelta
@@ -157,3 +159,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
