31fc2a45a0db15ea7be3742b73dc70b96383c3da
David Barbosa <davidbarbosa.dev@gmail.com>
2021-10-08 20:47:02 -0500

    [GET] List packs

--- agencyApp/views/__init__.py
+++ agencyApp/views/__init__.py
@@ -1,6 +1,7 @@
 from .userCreateView import UserCreateView
 from .userDetailView import UserDetailView
 
+from .packGetListView import PackGetListView
 from .packCreateView import PackCreateView
 
 from .purchaseCreateView import PurchaseCreateView
